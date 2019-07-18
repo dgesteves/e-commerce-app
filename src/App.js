d54003be35e4e0d77d8683de1ad1934fd1e4c001
diogo esteves <diogo.esteves.goncalves@gmail.com>
2019-07-18 20:57:49 +0100

    Home page Skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Home from './pages/Home'
+
+
 const App = () => {
     return (
         <div>
-            Hello World!
+            <Home/>
         </div>
     )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import './Home.scss'
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <div className='directory-menu'>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SNEAKERS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMAN'S</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MEN'S</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
