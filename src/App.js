627d7c7d1ab3ef0ddbfa4af52ce0621e78c0e9e6
diogo esteves <diogo.esteves.goncalves@gmail.com>
2019-07-19 23:19:38 +0100

    #App.css changed #Added header to app #added header component and styles

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import './App.css'
 import Shop from './pages/Shop/Shop'
+import Header from './components/Header/Header'
 
 const App = () => {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path={ '/' } component={ Home }/>
         <Route exact path={ '/shop' } component={ Shop }/>
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -3,12 +3,12 @@ import { withRouter } from 'react-router-dom'
 
 import './MenuItem.scss'
 
-const MenuItem = ({ title, image, size, history, linkUrl, match }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div className={ `${ size } menu-item` }
          onClick={ () => history.push(`${ match.url }${ linkUrl }`) }>
       <div className='background-image'
-           style={ { backgroundImage: `url(${ image })` } }/>
+           style={ { backgroundImage: `url(${ imageUrl })` } }/>
       <div className="content">
         <h1 className="title">{ title.toUpperCase() }</h1>
         <span className="subtitle">SHOP NOW</span>
